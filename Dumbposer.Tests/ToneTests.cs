815db418fdef826745faa7aab501c4efedeb5670
matthew.watkins <matthew.watkins@channeladvisor.com>
2018-03-31 18:28:02 -0400

    Better work on tones

---
+++ Dumbposer.Tests/ToneTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Dumbposer.Tests
+{
+    public class ToneTests
+    {
+        private Tone HOME_TONE = new Tone
+        {
+            Octave = 4,
+            Number = 9
+        };
+
+        [Fact]
+        public void Tone_HappyPath_StoredFields_Test()
+        {
+            Assert.Equal(4, HOME_TONE.Octave);
+            Assert.Equal(9, HOME_TONE.Number);
+        }
+
+        [Fact]
+        public void Tone_HappyPath_CalculatedFields_ID_Test()
+        {
+            Assert.Equal(49, HOME_TONE.ID);
+        }
+
+        [Fact]
+        public void Tone_HappyPath_CalculatedFields_Name_Test()
+        {
+            Assert.Equal("A", HOME_TONE.Name);
+        }
+
+        [Fact]
+        public void Tone_HappyPath_CalculatedFields_FullName_Test()
+        {
+            Assert.Equal("A4", HOME_TONE.FullName);
+        }
+    }
+}
---
+++ Dumbposer/Tone.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Dumbposer
+{
+    public class Tone
+    {
+        readonly static string[][] TONE_NAMES = new string[][]
+        {
+            new [] { "C" },
+            new [] { "C#", "Db" },
+            new [] { "D" },
+            new [] { "D#", "Eb" },
+            new [] { "E" },
+            new [] { "F" },
+            new [] { "F#", "Gb" },
+            new [] { "G" },
+            new [] { "G#", "Ab" },
+            new [] { "A" },
+            new [] { "A#", "Bb" },
+            new [] { "B" }
+        };
+
+        public int Octave { get; set; }
+        public int Number { get; set; }
+
+        public string Name
+        {
+            get => TONE_NAMES[Number][0];
+            set => Number = GetToneNumberFromName(value);
+        }
+
+        public int ID
+        {
+            get => (Octave * 12) + Number - 8;
+            set
+            {
+                var relativeID = value - 8;
+                Octave = relativeID / 12;
+                Number = relativeID % 12;
+            }
+        }
+
+        public string FullName
+        {
+            get => $"{Name}{Octave}";
+            set
+            {
+                Name = value.Substring(0, value.Length - 1);
+                Octave = value[value.Length - 1];
+            }
+        }
+
+        #region Private helpers
+
+        int GetToneNumberFromName(string value)
+        {
+            for (var i = 0; i < TONE_NAMES.Length; i++)
+            {
+                var toneNames = TONE_NAMES[i];
+                for (var j = 0; j < toneNames.Length; j++)
+                {
+                    if (toneNames[j] == value)
+                    {
+                        return i;
+                    }
+                }
+            }
+            throw new Exception($"Tone not found: {value}");
+        }
+
+        #endregion
+    }
+}
